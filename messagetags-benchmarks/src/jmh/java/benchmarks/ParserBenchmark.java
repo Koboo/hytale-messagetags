72cd08f0f38e3eebdfe23d9d0030b15cb4029823
Koboo <14143908+Koboo@users.noreply.github.com>
2026-02-11 16:02:50 +0100

    Create ParserBenchmark

---
+++ messagetags-benchmarks/src/jmh/java/benchmarks/ParserBenchmark.java
@@ -0,0 +1,54 @@
+package benchmarks;
+
+import com.hypixel.hytale.server.core.Message;
+import eu.koboo.messagetags.api.MessageTags;
+import fi.sulku.hytale.TinyMsg;
+import io.github.insideranh.talemessage.TaleMessage;
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OperationsPerInvocation;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 5, time = 2)
+@Measurement(iterations = 5, time = 3)
+@Fork(3)
+@State(Scope.Thread)
+public class ParserBenchmark {
+
+    @Param({
+        "Hello World!",
+        "<bold><italic><underline>Hello World</underline></italic></bold>",
+        "<bold>Aw<underline>eso</underline>me <color:#fcba03>ben<white>chm<red>ar<i>k</i> <color:dark_red>, but <link:https://github.com/Koboo/>ho</link>w<reset> fast <i>a</i>r<monospace>e</monospace>we really?",
+        "<transition:blue:red:yellow:0.5>Hello <gradient:blue:red:yellow>World!<gradient></transition>",
+    })
+    private String input;
+
+    @OperationsPerInvocation(100)
+    @Benchmark
+    public void parseMessageTags(Blackhole blackhole) {
+        blackhole.consume(MessageTags.parse(input));
+    }
+
+    @OperationsPerInvocation(100)
+    @Benchmark
+    public void parseTinyMessages(Blackhole blackhole) {
+        blackhole.consume(TinyMsg.parse(input));
+    }
+
+    @OperationsPerInvocation(100)
+    @Benchmark
+    public void parseTaleMessage(Blackhole blackhole) {
+        blackhole.consume(TaleMessage.parse(input));
+    }
+}

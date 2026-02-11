029a7f6e2965565c996768400c4162e643405f45
Koboo <admin@koboo.eu>
2026-02-11 18:42:15 +0100

    Update benchmarks

--- messagetags-benchmarks/src/jmh/java/benchmarks/ParserBenchmark.java
+++ messagetags-benchmarks/src/jmh/java/benchmarks/ParserBenchmark.java
@@ -1,50 +1,43 @@
 package benchmarks;
 
-import com.hypixel.hytale.server.core.Message;
 import eu.koboo.messagetags.api.MessageTags;
 import fi.sulku.hytale.TinyMsg;
 import io.github.insideranh.talemessage.TaleMessage;
 import java.util.concurrent.TimeUnit;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OperationsPerInvocation;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Param;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
+
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
-@BenchmarkMode({Mode.AverageTime, Mode.Throughput})
+@BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @Warmup(iterations = 5, time = 2)
 @Measurement(iterations = 5, time = 3)
-@Fork(3)
+@Fork(4)
 @State(Scope.Thread)
+@OperationsPerInvocation(100)
 public class ParserBenchmark {
 
     @Param({
-        "<bold><italic><underline>Hello World</underline></italic></bold>",
-        "<bold>Aw<underline>eso</underline>me <color:#fcba03>ben<white>chm<red>ar<i>k</i> <color:dark_red>, but how<reset> fast <i>a</i>r<monospace>e</monospace>we really?",
+        "Hello World",
+        "<bold>Hello World",
+        "<color:red>Hello World",
+        "<bold><italic><underline><color:red>Hello World</underline></italic></bold>",
+        "Hello <bold>world</bold>, this is a <color:yellow>yellow <italic>italic</italic></color> test.",
+        "<bold>Start</bold> of <italic>a long <color:blue>nested <underline>message</underline> with multiple</color> tags</italic> to <color:green>stress-test</color> the parser.",
+        "<bold>Aw<underline>eso</underline>me <color:#fcba03>ben<white>chm<red>ar<i>k</i> <color:dark_red>, but how<reset> fast <i>a</i>r<monospace>e</monospace>we really?"
     })
     private String input;
 
-    //@OperationsPerInvocation(100)
     @Benchmark
     public void parseMessageTags(Blackhole blackhole) {
         blackhole.consume(MessageTags.parse(input));
     }
 
-    //@OperationsPerInvocation(100)
     @Benchmark
     public void parseTinyMessages(Blackhole blackhole) {
         blackhole.consume(TinyMsg.parse(input));
     }
 
-    //@OperationsPerInvocation(100)
     @Benchmark
     public void parseTaleMessage(Blackhole blackhole) {
         blackhole.consume(TaleMessage.parse(input));

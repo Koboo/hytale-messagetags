0bb5483d4376d8e0bdbd38628542f8e737d69973
Koboo <admin@koboo.eu>
2026-02-11 18:41:12 +0100

    Remove legacy tags from unit-tests

--- messagetags-api/test/eu/koboo/messagetags/api/tests/TextStripTest.java
+++ messagetags-api/test/eu/koboo/messagetags/api/tests/TextStripTest.java
@@ -10,14 +10,14 @@ public class TextStripTest {
     @Test
     public void test1() {
         String expected = "Awesome test But everything stripped";
-        String tagText = "<bold>Aw<ul>e<&6>so</ul>me <color:#fcba03>test <color:&4>But every<r>thing <em>stripped";
+        String tagText = "<bold>Aw<ul>e<&6>so</ul>me <color:#fcba03>test <color:&4>But every<r>thing <underline>stripped";
         assertStripped(expected, MessageTags.stripToString(tagText));
     }
 
     @Test
     public void test2() {
         String expected = "server.commands.help.desc Test Translations\n";
-        String tagText = "<&b><link:https://github.com/Koboo><lang:server.commands.help.desc></link> Test <&a>Translations<newline>";
+        String tagText = "<&b><link:https://github.com/Koboo><lang:server.commands.help.desc></link> Test <&a>Translations<linebreak>";
         assertStripped(expected, MessageTags.stripToString(tagText));
     }
 

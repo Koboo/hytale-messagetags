aa3b0452286d867971507f0c824a75a4befc4777
Koboo <admin@koboo.eu>
2026-02-12 16:13:44 +0100

    Refactor NamedColor back to record

--- messagetags-api/src/eu/koboo/messagetags/api/colors/NamedColor.java
+++ messagetags-api/src/eu/koboo/messagetags/api/colors/NamedColor.java
@@ -1,39 +1,38 @@
 package eu.koboo.messagetags.api.colors;
 
-import java.util.Locale;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import java.util.Locale;
 
-public class NamedColor{
+public record NamedColor(@Nonnull String name, @Nonnull String hexCode) {
 
-    @Nonnull
-    public final String name;
-    public final char colorCode;
-    @Nonnull
-    public final String hexCode;
-    public final int value;
-
-    public NamedColor(String name, char colorCode, String hexCode) {
+    public NamedColor(String name, String hexCode) {
         this.name = name.toLowerCase(Locale.ROOT).trim();
-        this.colorCode = colorCode;
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty!");
+        }
         this.hexCode = hexCode.toLowerCase(Locale.ROOT).trim();
-        this.value = ColorUtils.hexToRGB(hexCode);
+        if (hexCode.isEmpty()) {
+            throw new IllegalArgumentException("HexCode cannot be empty!");
+        }
+        if (hexCode.length() == 6) {
+            throw new IllegalArgumentException("HexCode has to be prefixed with '#'");
+        }
     }
 
-    public static final NamedColor Black = new NamedColor("Black", '0', "#000000");
-    public static final NamedColor DarkBlue = new NamedColor("DarkBlue", '1', "#0000AA");
-    public static final NamedColor DarkGreen = new NamedColor("DarkGreen", '2', "#00AA00");
-    public static final NamedColor DarkAqua = new NamedColor("DarkAqua", '3', "#00AAAA");
-    public static final NamedColor DarkRed = new NamedColor("DarkRed", '4', "#AA0000");
-    public static final NamedColor DarkPurple = new NamedColor("DarkPurple", '5', "#AA00AA");
-    public static final NamedColor Gold = new NamedColor("Gold", '6', "#FFAA00");
-    public static final NamedColor Gray = new NamedColor("Gray", '7', "#AAAAAA");
-    public static final NamedColor DarkGray = new NamedColor("DarkGray", '8', "#555555");
-    public static final NamedColor Blue = new NamedColor("Blue", '9', "#5555FF");
-    public static final NamedColor Green = new NamedColor("Green", 'a', "#55FF55");
-    public static final NamedColor Aqua = new NamedColor("Aqua", 'b', "#55FFFF");
-    public static final NamedColor Red = new NamedColor("Red", 'c', "#FF5555");
-    public static final NamedColor LightPurple = new NamedColor("LightPurple", 'd', "#FF55FF");
-    public static final NamedColor Yellow = new NamedColor("Yellow", 'e', "#FFFF55");
-    public static final NamedColor White = new NamedColor("White", 'f', "#FFFFFF");
+    public static final NamedColor Black = new NamedColor("Black", "#000000");
+    public static final NamedColor DarkBlue = new NamedColor("Dark_Blue", "#0000AA");
+    public static final NamedColor DarkGreen = new NamedColor("Dark_Green", "#00AA00");
+    public static final NamedColor DarkAqua = new NamedColor("Dark_Aqua", "#00AAAA");
+    public static final NamedColor DarkRed = new NamedColor("Dark_Red", "#AA0000");
+    public static final NamedColor DarkPurple = new NamedColor("Dark_Purple", "#AA00AA");
+    public static final NamedColor Gold = new NamedColor("Gold", "#FFAA00");
+    public static final NamedColor Gray = new NamedColor("Gray", "#AAAAAA");
+    public static final NamedColor DarkGray = new NamedColor("Dark_Gray", "#555555");
+    public static final NamedColor Blue = new NamedColor("Blue", "#5555FF");
+    public static final NamedColor Green = new NamedColor("Green", "#55FF55");
+    public static final NamedColor Aqua = new NamedColor("Aqua", "#55FFFF");
+    public static final NamedColor Red = new NamedColor("Red", "#FF5555");
+    public static final NamedColor LightPurple = new NamedColor("Light_Purple", "#FF55FF");
+    public static final NamedColor Yellow = new NamedColor("Yellow", "#FFFF55");
+    public static final NamedColor White = new NamedColor("White", "#FFFFFF");
 }
